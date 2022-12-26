93bdb5869a66da30f5a23b116d3120c846cb6cef
Tapan B.K <bktapan16@gmail.com>
2022-12-26 11:37:09 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Todo from "./components/Todo";
+
+
+function App() {
+  return(
+     <div>
+       <h1>My Todos</h1>
+         <Todo text='Learn React'/>
+         <Todo text='Master React'/>
+         <Todo text='Explore the full React course'/>
+         <Todo text='Learn Master Git Course'/>
+     </div>
+  );
+}
+
+export default App;
---
+++ src/components/Todo.js
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+import Modal from "./Modal";
+import Backdrop from "./Backdrop";
+
+function Todo(props) {
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    function deleteHandler(){
+        setModalIsOpen(true);
+    }
+
+    function closeModalHandler(){
+        setModalIsOpen(false);
+    }
+
+    function confirmModalHandler(){
+        setModalIsOpen(false);
+    }
+
+    return (
+        <div className='card'>
+        <h2>{props.text}</h2>
+        <div className="actions">
+            <button className='btn' onClick={deleteHandler}>DELETE</button>
+        </div>
+            {modalIsOpen &&  <Modal onCancel={closeModalHandler} onConfirm={confirmModalHandler} />}
+            {modalIsOpen && <Backdrop onClick={closeModalHandler}/>}
+    </div>);
+}
+
+export default Todo;
